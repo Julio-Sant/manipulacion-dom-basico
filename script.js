de2f27de7f5ed632e300a1e25f6dcf0d7aa6daa7
Julio-Sant <julio.santana21@gmail.com>
2023-03-14 15:10:04 -0300

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('Submit', sumarInputValues);
+
+
+function sumarInputValues(event){
+    console.log({event});
+    event.preventdefault();    
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+
+
+
+
